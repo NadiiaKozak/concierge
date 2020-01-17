cf9fd31d85b3019cc913761f0f6c9c69c0078da3
Nadiia Kozak <lv191177anv@gmail.com>
2020-01-17 23:00:07 +0200

    add file HW Django ORM

--- app/concierge/mycore/admin.py
+++ app/concierge/mycore/admin.py
@@ -2,7 +2,7 @@
 from django.conf import settings
 from django.contrib import admin
 
-from .models import Tenant
+from .models import Tenant, Journal, Room
 
 
 @admin.register(Tenant)
@@ -10,3 +10,17 @@ class TenantAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'phone')
     search_fields = ('first_name', 'last_name', 'phone', 'date_of_birth')
     list_filter = ('date_of_birth', 'last_name')
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('number_room', 'max_tenants', 'status')
+    search_fields = ('number_room', 'max_tenants', 'status')
+    list_filter = ('number_room', 'max_tenants', 'status')
+
+
+@admin.register(Journal)
+class JournalAdmin(admin.ModelAdmin):
+    list_display = ('room_id', 'tenant_id', 'tenants', 'key_in', 'key_out')
+    search_fields = ('room_id', 'tenant_id', 'tenants', 'key_in', 'key_out')
+    list_filter = ('room_id', 'tenant_id', 'tenants', 'key_in', 'key_out')
--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,4 @@
-create user concierge_dev password 'simplepass';
+create user concierge_dev password 'concierge_dev';
 
 create database concierge_db encoding 'utf-8';
 grant all privileges on database concierge_db to concierge_dev;
